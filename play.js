ce5413c67ee37d570bd747c29808a946ba3f0273
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-09-28 18:29:45 +0000

    Added the setupInput and handleUserInput to establish UI

--- play.js
+++ play.js
@@ -1,3 +1,20 @@
-const { connect } = require("./client")
+const { connect } = require("./client");
 
 connect();
+
+const handleUserInput = function(key) {
+    if (key === '\u0003') {
+      process.exit();
+    }
+};
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+setupInput();

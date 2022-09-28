d33b03fd5375d99e2b8ddd37dd29a409ab8b3936
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-09-28 23:30:58 +0000

    Added a command line argument to add the player's name

--- client.js
+++ client.js
@@ -1,16 +1,18 @@
 const net = require("net");
+const { IP, PORT } = require('./constants');
+const initials = (process.argv)[2];
 
 const connect = function() {
   const conn = net.createConnection({
-    host: "165.227.47.243",
-    port: 50541,
+    host: IP,
+    port: PORT,
   });
 
   conn.setEncoding("utf-8");
 
   conn.on('connect', () => {
     console.log("Successfully connected to game server");
-    conn.write("Name: JYJ");
+    conn.write(`Name: ${initials}`);
   });
 
   conn.on('data', (data) => {

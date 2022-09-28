cbdb76d98e412e0781ad41fd03238d8c280578d4
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-09-28 23:30:13 +0000

    Created a constants file containing important constants and exported them

---
+++ constants.js
@@ -0,0 +1,16 @@
+const IP = "165.227.47.243";
+const PORT = 50541;
+const controls = {
+  'w': 'Move: up',
+  'a': 'Move: left',
+  's': 'Move: down',
+  'd': 'Move: right',
+  ' ': 'Say: :)',
+  'q': 'Say: sewy!'
+}
+
+module.exports = {
+  IP,
+  PORT,
+  controls
+};

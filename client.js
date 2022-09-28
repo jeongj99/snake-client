50c4c70464ba35b225c567fd836d7340a336f58d
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-09-28 18:12:25 +0000

    Tried to send the move up string to the server, then deleted the hard code

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 const net = require("net");
 
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
     host: "165.227.47.243",
     port: 50541,
@@ -10,16 +10,16 @@ const connect = function () {
 
   conn.on('connect', () => {
     console.log("Successfully connected to game server");
-    conn.write("Name: JYJ")
-  })
+    conn.write("Name: JYJ");
+  });
 
   conn.on('data', (data) => {
-    console.log(data)
-  })
+    console.log(data);
+  });
 
   return conn;
-}
+};
 
 module.exports = {
   connect
-}
+};
